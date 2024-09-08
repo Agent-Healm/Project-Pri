2400bd53659cc1aa0ef39557e7146bb56129f902
Healm <116085297+Agent-Healm@users.noreply.github.com>
2024-09-08 10:58:05 +0700

    Renamed 'spawn' -> 'Room Generation'.

--- RoomGeneration.cs
+++ RoomGeneration.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class spawn : MonoBehaviour
+public class RoomGeneration : MonoBehaviour
 {
     public GameObject[] objects;
     // Start is called before the first frame update
