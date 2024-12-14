0f88194a99ffa6d282618a8ef3cad5fba01d771a
Healm <116085297+Agent-Healm@users.noreply.github.com>
2024-12-15 00:11:26 +0800

    bullet now disappear when hitting target.

--- Class/Bullet.cs
+++ Class/Bullet.cs
@@ -2,16 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet
+public class Bullet : MonoBehaviour
 {
     public Vector2 direction;
     public GameObject texture;
+    public float uptime = 3.0f;
 
-    // public Bullet(Vector2 dir, GameObject texture){
-    //     this.direction = dir;
-    //     this.texture = texture;
-    // }
-    // public Bullet(GameObject texture){
-    //     this.texture = texture;
-    // }
+    private float _time;
+
+    void Awake(){
+        // set framerate limit
+        Application.targetFrameRate = 30;
+    }
+    void Start(){
+
+    }
+    void Update(){
+        if ( _time >= uptime){
+            Destroy(gameObject);
+        }
+        _time += 0.03f;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        // Debug.Log("hit");
+        if (collision.gameObject.name == "home"){
+            Debug.Log("I hit something");
+            Destroy(gameObject);
+        }
+    }
 }
